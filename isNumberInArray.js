e47b78f3d24fd5a6b45d8aacbf3a85aafd904dba
DmitriyGavrilov <gavrilovdmitriy@i.ua>
2022-12-29 12:57:33 +0200

    Add files via upload

---
+++ isNumberInArray.js
@@ -0,0 +1,10 @@
+function check(a, x) {
+  let answer = false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === x) {
+      answer = true;
+    }
+  }
+  return answer;
+  // your code here
+}
